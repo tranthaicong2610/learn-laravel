bb53b154d90ff7b48fc06b91527cc7c51c232c13
tranthaicong2610 <69552670+tranthaicong2610@users.noreply.github.com>
2021-12-20 08:44:26 +0700

    dinh danh- group

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,29 @@ Route::get
 Route::get("hello/ok",function(){
     echo "<h1>chao cai gi </h1>";
 });
-Route::get("/name/{name}",function($name){// truyen tham so tren url
+Route::get("name/{name}",function($name){// truyen tham so tren url
     echo "ten ban la :".$name;
 })->where(['name'=>'[a-z]+']);//regular expression
+// dinh danh (dat ten)
+Route::get("dinhdanh",['as'=>'myroute',function(){
+    echo "dinh danh cho route";
+}]);
+Route::get("dinhdanh2",function(){
+    echo "ban da dat ten ";
+})->name("route2");
+
+// goi ten
+Route::get("goiten",function(){
+    return redirect()->route("route2");
+});
+// nhom route
+Route::group(['prefix'=>'myroute'],function(){
+    route::get("user1",function(){
+        echo "user1";
+    });
+    route::get("user2",function(){
+        echo "user2";
+    });
+
+});
+
