16a052062306dc505c694576df118870577e6bf4
tranthaicong2610 <69552670+tranthaicong2610@users.noreply.github.com>
2022-01-16 16:45:34 +0700

    route 1

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+
 class MyController extends Controller
 {
     //
@@ -14,7 +15,7 @@ class MyController extends Controller
     // nhan tham so tu route
     public function thamso($name)
     {
-        echo "tham so chuyen :".$name;
+        echo "tham so chuyen :" . $name;
     }
     // goi sang 1 route khac
     public function chuyen_route()
@@ -23,40 +24,38 @@ class MyController extends Controller
     }
     public function ham_request(Request $request)
     {
-        echo $request->path()."<br/>";
-        echo $request->url()."<br/>";
+        echo $request->path() . "<br/>";
+        echo $request->url() . "<br/>";
         //kiem tra phuong thuc
-        if($request->isMethod('get'))
-        {
+        if ($request->isMethod('get')) {
             echo "phuong thuc get <br/>";
-        }
-        else echo "khong phai phuong thuc get <br/>";
+        } else echo "khong phai phuong thuc get <br/>";
 
         // kiem tra duong dan co chua chu can tim khong
-        if($request->is('goi*')) echo "co chua chu can tim <br/>";
+        if ($request->is('goi*')) echo "co chua chu can tim <br/>";
         else "khong chua chu can tim trong duong dan ";
     }
     public function xuly(Request $request)
     {
-        if($request->username=="tran thai cong"&&$request->password=="123456")
-        echo "ban dang nhap thanh cong<br/>";
+        if ($request->username == "tran thai cong" && $request->password == "123456")
+            echo "ban dang nhap thanh cong<br/>";
         else echo "ban dang nhap that bai<br/> ";
 
         // tim xem co tham so hien huu hay khong
-        if($request->has('name')) echo "co <br/>";
+        if ($request->has('name')) echo "co <br/>";
         else echo "khong <br/>";
-        if($request->has('username')) echo "ban da nhap username<br/>";
+        if ($request->has('username')) echo "ban da nhap username<br/>";
         else echo "ban chua nhap username<br/>";
-
-   }
-   public function setCookie()
-   {
-       $response=new Response();
-       $response->withCookie('khoahoc','lap trinh laravel',1);
-       return $response;
-   }
-   public function getCookie(Request $request)
-   {
-       return $request->cookie('khoahoc');
-   }
+    }
+    public function set_cookie()
+    {
+        $res = new Response();
+        $res->withCookie(cookie('ten_cookie', 'gia_tri', 0.1));
+        return $res;
+    }
+    public function get_cookie(Request $req)
+    {
+        $val = $req->cookie('ten_cookie');
+        echo $val;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -67,6 +68,68 @@ route::get("getform",function(){
 // xu ly login
 route::get("xuly",[MyController::class,'xuly'])->name("xuly");
 //goi ham cookie
-route::get('setCookie',[MyController::class,'setCookie']);
-route::get('getCookie',[MyController::class,'getCookie']);
+route::get('setCookie',[MyController::class,'set_cookie']);
+route::get('getCookie',[MyController::class,'get_cookie']);
 
+route::get("hello",[UserController::class,'index']);
+// Bộ định tuyến cho phép bạn đăng ký các tuyến đáp ứng với bất kỳ động từ HTTP nào:
+
+// Route::get($uri, $callback);
+// Route::post($uri, $callback);
+// Route::put($uri, $callback);
+// Route::patch($uri, $callback);
+// Route::delete($uri, $callback);
+// Route::options($uri, $callback);
+
+// Đôi khi bạn có thể cần đăng ký một tuyến đáp ứng với nhiều động từ HTTP.
+//  Bạn có thể làm như vậy bằng cách sử dụng matchphương pháp này. Hoặc, bạn thậm chí
+// có thể đăng ký một tuyến đáp ứng tất cả các động từ HTTP bằng anyphương pháp:
+
+// Route::match(['get', 'post'], '/', function () {
+//     //
+// });
+
+// Route::any('/', function () {
+    //
+// });
+// Route::redirect('/here', '/there');
+// Theo mặc định, Route::redirecttrả về 302mã trạng thái. Bạn có thể tùy chỉnh mã trạng thái bằng cách sử dụng tham số thứ ba tùy chọn:
+
+// Route::redirect('/here', '/there', 301);
+// Hoặc, bạn có thể sử dụng Route::permanentRedirectphương pháp để trả về 301mã trạng thái:
+
+// Route::permanentRedirect('/here', '/there');
+// Xem các tuyến đường
+// Nếu tuyến đường của bạn chỉ cần trả về một chế độ xem , bạn có thể sử dụng Route::viewphương pháp này.
+//  Giống như redirectphương pháp này, phương pháp này cung cấp một phím tắt đơn giản để bạn không phải xác định toàn bộ tuyến đường hoặc bộ điều khiển.
+//   Phương viewthức chấp nhận một URI làm đối số đầu tiên của nó và một tên dạng xem là đối số thứ hai của nó.
+//   Ngoài ra, bạn có thể cung cấp một mảng dữ liệu để chuyển tới dạng xem dưới dạng đối số thứ ba tùy chọn:
+
+// Route::view('/welcome', 'welcome');
+
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+
+// Tham số tuyến đường
+// Các thông số bắt buộc
+// Đôi khi bạn sẽ cần nắm bắt các phân đoạn của URI trong tuyến đường của mình. Ví dụ: bạn có thể cần lấy ID của người dùng từ URL. Bạn có thể làm như vậy bằng cách xác định các thông số tuyến đường:
+
+// Route::get('/user/{id}', function ($id) {
+//     return 'User '.$id;
+// });
+// Bạn có thể xác định bao nhiêu tham số tuyến đường theo yêu cầu của tuyến đường của bạn:
+
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     //
+// });
+// Tham số tuyến đường luôn được đặt trong {}dấu ngoặc nhọn và phải bao gồm các ký tự chữ cái.
+//  Dấu gạch dưới ( _) cũng được chấp nhận trong tên tham số tuyến đường. Các tham số định tuyến được đưa vào các lệnh gọi lại / bộ điều khiển tuyến dựa trên thứ tự của chúng - tên của các đối số bộ điều khiển / gọi lại tuyến không quan trọng.
+route::get('/hello/{id}',function($id){
+    return "hello".$id;
+});
+// Tham số & Tiêm phụ thuộc
+// Nếu tuyến của bạn có các phần phụ thuộc mà bạn muốn vùng chứa dịch vụ Laravel tự động đưa vào lệnh gọi lại của tuyến,
+//  bạn nên liệt kê các thông số tuyến sau các phần phụ thuộc của mình:
+Route::get('/user/{id}', function (Request $request, $id) {
+    return 'User '.$id;
+});
